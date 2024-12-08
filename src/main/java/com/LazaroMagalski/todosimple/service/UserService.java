d1cf73ca4fb2e04bae626b4ca9bfbe44f12a34e8
Lazaro M <lazaro.magalski@gmail.com>
2024-12-08 16:53:52 -0300

    Refactor UserService

--- src/main/java/com/LazaroMagalski/todosimple/service/UserService.java
+++ src/main/java/com/LazaroMagalski/todosimple/service/UserService.java
@@ -17,9 +17,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
             return user.orElseThrow(() -> new RuntimeException(
@@ -30,7 +27,6 @@ public class UserService {
     public User create(User obj){
         obj.setId(null);//segurança, o usuario sempre criar nulo
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
